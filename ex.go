6395745f41bd09ce46691fb8d241eb137287d29b
saur4ig <saurons909@gmail.com>
2020-05-07 15:12:10 +0300

    added example

--- ex.go
+++ ex.go
@@ -1,10 +1,11 @@
-# raketa
-[![Build Status](https://travis-ci.org/Saur4ig/raketa.svg?branch=master)](https://travis-ci.org/Saur4ig/raketa)
-[![Coverage Status](https://coveralls.io/repos/github/Saur4ig/raketa/badge.svg?branch=master)](https://coveralls.io/github/Saur4ig/raketa?branch=master)  
-Rocket.Chat client for messages and notifications, inspired by `https://github.com/detached/gorocket/`
+package raketa
 
-Base example:
-```go
+import (
+	"net/http"
+
+	"github.com/Saur4ig/raketa/rest"
+)
+
 func main() {
 	httpClient := &http.Client{}
 	// Create rest client
@@ -29,4 +30,3 @@ func main() {
 	// check err in real project
 	_ = restClient.SendMessage(rest.Message{MessageText: "hello"})
 }
-```
